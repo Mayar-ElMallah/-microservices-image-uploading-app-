a7a1c900d28c23ad46f99bca0655ad8d48020a2b
Mayar-ElMallah <54380002+Mayar-ElMallah@users.noreply.github.com>
2022-09-16 15:07:32 +0200

    Update model.index.ts

--- udagram-api-feed/src/controllers/v0/model.index.ts
+++ udagram-api-feed/src/controllers/v0/model.index.ts
@@ -1,3 +1,3 @@
-import { FeedItem } from './feed/models/FeedItem';
+import { Feed_Item } from './feed/models/Feed_Item';
 
-export const V0MODELS = [ FeedItem ];
+export const V0MODELS = [ Feed_Item ];
